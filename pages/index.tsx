6885010e7bb03f9ed58731e6bccf42d4971226e3
Jellyfish25 <mitkodimitrov50@gmail.com>
2022-10-10 22:43:00 -0700

    updated starting page and began implementing customerHome page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,23 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import { Form } from 'react-bootstrap';
 
 const Home: NextPage = () => {
+  const { Group } = Form;
   return (
     <div className={styles.container}>
+      <Group className='mb-3'>
       Pick One:
+      <br></br>
       <br></br>
       <Link href='/register'>REGISTER</Link>
       <br></br>
+      <br></br>
       <Link href='/login'>LOGIN</Link>
+      </Group>
     </div>
+    
   );
 };
 
