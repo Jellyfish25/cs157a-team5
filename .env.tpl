21ef6bd7805b327199329fd4b2a28433cac7def5
Logan Dang <logandang100@gmail.com>
2022-10-11 12:17:28 -0700

    install mysql package

---
+++ .env.tpl
@@ -0,0 +1,5 @@
+DB_SCHEMA=
+DB_USERNAME=
+DB_PASSWORD=
+# In terminal: cp .env.tpl .env
+# Then fill in env variables (Ex. DB_SCHEMA=team5)
---
+++ database/index.ts
@@ -0,0 +1,26 @@
+import mysql from 'serverless-mysql'
+
+const db = mysql({
+  config: {
+    host: process.env.DB_SCHEMA || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'password'
+  }
+})
+
+// Initialize db
+db.query('CREATE DATABASE IF NOT EXISTS team5')
+db.query('USE team5')
+
+/**
+ * 
+ * @param queryStr the query to execute
+ * @returns 
+ */
+const query = async (queryStr: string) => {
+  const result = await db.query(queryStr);
+  await db.end()
+  return result;
+}
+
+export default query
