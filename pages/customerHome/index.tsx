a650a40855988e1c4270abf9e35b83bfa6d0f24f
Jellyfish25 <mitkodimitrov50@gmail.com>
2022-11-08 21:32:36 -0800

    updated page format

--- pages/customerHome/index.tsx
+++ pages/customerHome/index.tsx
@@ -6,27 +6,32 @@ export default function CustomerHome() {
   const { Group, Label, Control, Select } = Form;
   return (
     <section className={styles.formWrapper}>
-      <section className={styles.h1Styles}>
-        <Group className='mb-3' controlId='displayOptions'>
-          <Label>Display Options</Label>
-          <Select aria-label='UserType'>
-            <option value='Games'>Games</option>
-            <option value='Movies'>Movies</option>
-            <option value='Books'>Books</option>
-            <option value='Music'>Music</option>
-          </Select>
-        </Group>
-      </section>
-      <MediaTable
-      items={Array.from({ length: 10 }, (_, i) => ({
-        name: 'SpiderMan-' + i,
-        src: '/spider-man.webp',
-      }))}
-    />      
-    
-        <Group className='mb-3' controlId='formBasicEmail'>
-        <Control type='username' placeholder='Search for Media Title'></Control>
+      <Group className='mb-3' controlId='displayOptions'>
+        <Label>Display Options</Label>
+        <Select aria-label='UserType'>
+          <option value='Games'>Games</option>
+          <option value='Movies'>Movies</option>
+          <option value='Books'>Books</option>
+          <option value='Music'>Music</option>
+        </Select>
       </Group>
+
+      <section className={styles.h1Styles}>
+        <Group className='mb-3' controlId='formBasicEmail'>
+          <Control type='username' placeholder='Search for Media Title'></Control>
+        </Group>
+        <br></br>
+        <MediaTable
+          items={Array.from({ length: 10 }, (_, i) => ({
+            name: 'SpiderMan-' + i,
+            src: '/spider-man.webp',
+          }))}
+        />
+      </section>
+
+      <section>
+        Hello, Customer!
+      </section>
     </section>
   );
 }
