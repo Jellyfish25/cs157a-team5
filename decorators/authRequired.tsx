8a0e5c815ba0d3b70b4bb2b52fd8523d8be581fc
Jellyfish25 <mitkodimitrov50@gmail.com>
2022-11-24 11:15:16 -0800

    finished contentCreatorHome page

---
+++ decorators/authRequired.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { useContext, useEffect } from 'react';
+import { AuthContext } from '../hooks';
+
+export function roleRequired(
+  role: 'customer' | 'employee' | 'contentCreator',
+  Page: NextPage
+) {
+  return function ProtectedRoute() {
+    const user = useContext(AuthContext);
+    const router = useRouter();
+
+    useEffect(() => {
+      if (!user) {
+        window.location.replace('/login');
+      }
+    }, [user, router]);
+
+    return user ? <Page /> : null;
+  };
+}
--- pages/contentCreatorHome/index.tsx
+++ pages/contentCreatorHome/index.tsx
@@ -1,13 +1,42 @@
 import { MediaTable } from '../../components';
+import styles from './contentCreatorHome.module.css';
+import { Button, Form } from 'react-bootstrap';
 import { roleRequired } from '../../decorators';
+import Link from 'next/link';
+
 
 export default roleRequired('contentCreator', function ContentCreatorHome() {
+  const { Group, Label, Control, Select } = Form;
   return (
-    <MediaTable
-      items={Array.from({ length: 10 }, (_, i) => ({
-        name: 'SpiderMan-' + i,
-        src: '/spider-man.webp',
-      }))}
-    />
+    <section className={styles.formWrapper}>
+      <Group className='mb-3' controlId='displayOptions'>
+        <Group className={styles.fontStyle}>
+        <Link href='/createMedia'>Publish</Link>
+        <br></br>
+        <Link href='/deleteMedia'>Delete</Link>
+        <br></br>
+        <Label>Display Options</Label>
+        <Select aria-label='UserType'>
+          <option value='Games'>Games</option>
+          <option value='Movies'>Movies</option>
+          <option value='Books'>Books</option>
+          <option value='Music'>Music</option>
+        </Select>
+      </Group>
+      </Group>
+
+      <section className={styles.h1Styles}>
+        <Group className={styles.titleStyle}>
+          Your Media
+        </Group>
+        <br></br>
+        <MediaTable
+          items={Array.from({ length: 10 }, (_, i) => ({
+            name: 'SpiderMan-' + i,
+            src: '/spider-man.webp',
+          }))}
+        />
+      </section>
+    </section>
   );
 });
